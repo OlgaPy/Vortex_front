32899f4f3f0b638410486b6d61b0722479cc6771
chicobambuni <utorrentfilibusers@gmail.com>
2023-08-08 12:23:25 +0300

    Создан каркас для виджета SiteNavigationPanelWidget

--- src/widgets/SiteNavigationPanelWidget.vue
+++ src/widgets/SiteNavigationPanelWidget.vue
@@ -1,7 +1,18 @@
 <template>
-
+  <div class="container">
+  </div>
 </template>
 
 <style lang="scss" scoped>
-
+.container {
+  padding: 32px;
+  display: flex;
+  justify-content: start;
+  align-items: start;
+  flex-direction: column;
+  background: var(--ui-block-bg-color);
+  border-radius: 10px;
+  width: 300px;
+  height: 450px;
+}
 </style>
