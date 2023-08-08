68fc8bacbc3506936341be645c4288b3cd3f8881
chicobambuni <utorrentfilibusers@gmail.com>
2023-08-08 20:06:39 +0300

    Проработан виджет SiteInfoPanel

--- src/widgets/SiteInfoPanel.vue
+++ src/widgets/SiteInfoPanel.vue
@@ -2,7 +2,11 @@
 
 <template>
   <div :class="$style.container">
-
+    <router-link to="/">Помощь и поддержка</router-link>
+    <router-link to="/">О проекте</router-link>
+    <router-link to="/">Новости проекта</router-link>
+    <router-link to="/">Правила сообщества</router-link>
+    <router-link to="/">Контакты</router-link>
   </div>
 </template>
 
@@ -11,11 +15,16 @@
   padding: 32px;
   display: flex;
   justify-content: start;
-  align-items: center;
+  align-items: start;
   flex-direction: column;
   background: var(--ui-sub-main-color);
   border-radius: 10px;
   width: 100%;
   min-height: 241px;
+  gap: 14px;
+}
+.container > * {
+  font-size: 14px;
+  color: var(--ui-secondary-color);
 }
 </style>
