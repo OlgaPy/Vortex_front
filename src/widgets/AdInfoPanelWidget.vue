dbf86bec7238c6306a3cfecc4db5529acf74c7e2
chicobambuni <utorrentfilibusers@gmail.com>
2023-08-08 20:04:19 +0300

    Проработан виджет AdInfoPanelWidget

--- src/widgets/AdInfoPanelWidget.vue
+++ src/widgets/AdInfoPanelWidget.vue
@@ -4,7 +4,7 @@
 
 <template>
   <div :class="$style.container">
-
+    <router-link to="/">Реклама / инструменты модератора</router-link>
   </div>
 </template>
 
@@ -13,11 +13,15 @@
   padding: 32px;
   display: flex;
   justify-content: start;
-  align-items: center;
+  align-items: start;
   flex-direction: column;
   background: var(--ui-sub-main-color);
   border-radius: var(--border-radius-base);
-  min-width: 300px;
-  min-height: 241px;
+  width: 300px;
+  height: 241px;
+}
+.container > * {
+  font-size: 14px;
+  color: var(--ui-secondary-color);
 }
 </style>
