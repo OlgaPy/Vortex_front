391643c5508768c35f93055fcdf67c8b93592f47
chicobambuni <utorrentfilibusers@gmail.com>
2023-08-09 20:32:25 +0300

    Удалён use для quasar

--- src/app/index.ts
+++ src/app/index.ts
@@ -9,12 +9,7 @@ import {useThemeStore} from "@/shared/stores/ThemeStore";
 
 export {App}
 export const registerPlugins = (app: App<Element>): void => {
-    app.use(createPinia())
-        .use(router)
-        .use(Quasar, {
-            plugins: {}, // import Quasar plugins and add here
-            lang: quasarLang,
-        })
+    app.use(createPinia()).use(router);
 }
 
 
