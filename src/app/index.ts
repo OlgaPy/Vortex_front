d085044ce9815e1e1c1f330f2feb44eabc235056
chicobambuni <utorrentfilibusers@gmail.com>
2023-08-09 20:30:12 +0300

    Удалён quasar

--- src/app/index.ts
+++ src/app/index.ts
@@ -1,11 +1,3 @@
-// Import icon libraries
-import '@quasar/extras/roboto-font/roboto-font.css'
-import '@quasar/extras/material-icons/material-icons.css'
-// Import Quasar css
-import 'quasar/src/css/index.sass'
-import { Quasar } from 'quasar'
-import quasarLang from 'quasar/lang/ru'
-
 // Import App scss
 import './styles/index.scss'
 
