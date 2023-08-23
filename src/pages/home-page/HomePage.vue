c7a8709a00e32dd44838b4fc424da8467d0e47b1
chicobambuni <utorrentfilibusers@gmail.com>
2023-08-23 19:30:02 +0300

    Убрано переключение ориентации приложения с помощью .ts

--- src/pages/home-page/HomePage.vue
+++ src/pages/home-page/HomePage.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
-import {ApplicationOrientations, useApplicationOrientationStore} from "@/shared/stores/ApplicationOrientationStore";
 import HomeDesktopPage from "./ui/HomeDesktopPage.vue";
 import HomeMobilePage from "./ui/HomeMobilePage.vue";
-
-const appOrientation = useApplicationOrientationStore();
 </script>
 
 <template>
-	<HomeDesktopPage v-if="appOrientation.orientation === ApplicationOrientations.DESKTOP"/>
-	<HomeMobilePage v-else-if="appOrientation.orientation === ApplicationOrientations.MOBILE"/>
+	<div>
+		<div :class="$style.homeDesktopPage">
+			<HomeDesktopPage/>
+		</div>
+		<div :class="$style.homeMobilePage">
+			<HomeMobilePage/>
+		</div>
+	</div>
 </template>
+
+<style lang="scss" module>
+.homeDesktopPage, .homeMobilePage {
+	display: none;
+}
+
+@media screen and (min-width: 360px) and (max-width: 1280px) {
+	.homeMobilePage {
+		display: flex;
+	}
+}
+
+@media screen and (min-width: 1280px) {
+	.homeDesktopPage {
+		display: flex;
+	}
+}
+</style>
