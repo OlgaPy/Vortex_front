38ebce6893f3f379775ef723ad6e7ff7afb35428
PaulChernin <pavel@chernin.ru>
2023-08-25 17:36:00 +0300

    Добавлены стили для ToggleGlobalThemeFeature

--- src/features/ToggleGlobalThemeFeature.vue
+++ src/features/ToggleGlobalThemeFeature.vue
@@ -1,18 +1,83 @@
 <script setup lang="ts">
 import { useThemeStore, ThemeModes } from '@/shared/stores/ThemeStore'
-const { setThemeMode } = useThemeStore();
+import { storeToRefs } from 'pinia'
+import SunIcon from '@/shared/ui/icons/SunIcon.svg'
+import MoonIcon from '@/shared/ui/icons/MoonIcon.svg'
+
+const store = useThemeStore()
+const { themeMode } = storeToRefs(store)
+const { setThemeMode } = store
+
+const toggleThemeMode = () => {
+	const newMode = themeMode.value !== ThemeModes.DARK ? ThemeModes.DARK : ThemeModes.LIGHT
+	setThemeMode(newMode)
+}
 </script>
 
 <template>
-	<div :class="$style.container">
-		<button @click="setThemeMode(ThemeModes.LIGHT)">Light</button>
-		<button @click="setThemeMode(ThemeModes.DARK)">Dark</button>
-	</div>
+	<label
+		@click.prevent="toggleThemeMode"
+		:class="$style.container"
+	>
+		<input
+			type="checkbox"
+			:checked="themeMode === ThemeModes.DARK"
+			:class="$style.checkbox"
+		/>
+  	<span :class="$style.slider"></span>
+		<SunIcon
+			v-if="themeMode === ThemeModes.DARK"
+			:class="$style.sun_icon"
+		/>
+		<MoonIcon
+			v-else
+			:class="$style.moon_icon"
+		/>
+	</label>
 </template>
 
 <style module>
 .container {
-	display: flex;
-	gap: 10px;
+	position: relative;
+	height: 24px;
+	width: 48px;
+	border-radius: 12px;
+	cursor: pointer;
+	background-color: var(--color-gray-22);
+	box-shadow: inset 2px 0 4px var(--color-gray-8);
+}
+
+.checkbox {
+	width: 0;
+	height: 0;
+	appearance: none;
+}
+
+.slider {
+	position: absolute;
+	left: 2px;
+	top: 2px;
+	width: 20px;
+	height: 20px;
+	background-color: var(--color-primary-1);
+	border-radius: 50%;
+	box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.15),
+							0px 1px 2px rgba(0, 0, 0, 0.30);
+}
+
+.checkbox:checked + .slider {
+	left: calc(48px - 20px - 2px);
+}
+
+.moon_icon {
+	position: absolute;
+	top: 4px;
+	right: 4px;
+}
+
+.sun_icon {
+	position: absolute;
+	top: 4px;
+	left: 4px;
 }
 </style>
