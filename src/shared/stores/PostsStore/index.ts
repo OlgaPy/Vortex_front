299c36aa74460d873ada1fa50e4369fe0c9744f7
chicobambuni <utorrentfilibusers@gmail.com>
2023-09-04 19:29:04 +0300

    Добавлен TODO

--- src/shared/stores/PostsStore/index.ts
+++ src/shared/stores/PostsStore/index.ts
@@ -1,4 +1,6 @@
 import type { IPost } from './types';
 import { usePostsStore } from './PostsStore';
 
+//TODO Пересмотреть реализацию и расположение src/shared/stores/PostsStore
 export { IPost, usePostsStore };
+
